7b4b9fafd318e7cb43abbaea083afe332e7d3b04
Kaique Gama <kaiquesilva49@hotmail.com>
2023-03-11 23:13:58 -0300

    first endpoint

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using filmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmesApi.Controllers;
+
+[ApiController]
+[Route("/filmes")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void Create([FromBody]Filme filme)
+    {
+        filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public List<Filme> List()
+    {
+        return filmes;
+    }
+}
